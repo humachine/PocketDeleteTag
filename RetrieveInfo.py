fd820e34917d3a863b9ee35c7e2ef5baa12d0155
Humachine <swarun@gmail.com>
2016-12-09 10:32:44 -0800

    Saving Access Tokens to file

--- RetrieveInfo.py
+++ RetrieveInfo.py
@@ -1,36 +1,36 @@
-import requests
 import RetrieveInfoUtil as RetInfUtil
 
 CONSUMER_KEY = RetInfUtil.ReadConsumerKey()
-REQ_TOK_METHOD_URL = "https://getpocket.com/v3/oauth/request"
-# REDIRECT_URI = "tagDelete:authorizationFinished" TODO
-REDIRECT_URI = "http://www.google.com"
-AUTHORIZE_REQ_URL = "https://getpocket.com/auth/authorize?"
-ACCESS_TOK_REQ_URL = "https://getpocket.com/v3/oauth/authorize"
+REQ_TOK_METHOD_URL = 'https://getpocket.com/v3/oauth/request'
+REDIRECT_URI = 'http://www.google.com' #Change to application if using web application
+AUTHORIZE_REQ_URL = 'https://getpocket.com/auth/authorize?'
+ACCESS_TOK_REQ_URL = 'https://getpocket.com/v3/oauth/authorize'
 
-def BuildPayload(consumer_key, redirect_uri):
-    """Build Payload from Consumer Key & Redirect URL/Code"""
-    payload= {"consumer_key":consumer_key,
-            "redirect_uri":redirect_uri}
-    return payload
+def ObtainNewAccessToken(access_info_filename=''):
+    #Obtain a request token from Pocket
+    payload= {'consumer_key':CONSUMER_KEY, 'redirect_uri':REDIRECT_URI}
+    r = RetInfUtil.MakeRequest(REQ_TOK_METHOD_URL, payload)
+    REQUEST_TOKEN = RetInfUtil.ParseURLEncodedQuery(r.text)['code'][0]
 
-def MakeRequest(method_url, payload):
-    r = requests.post(method_url, data=payload)
-    return r
+    #Generate Access Token Request URL. The URL directs users to a page where they can authenticate the app
+    auth_req_url = RetInfUtil.BuildAccessTokReqURL(AUTHORIZE_REQ_URL, REQUEST_TOKEN, REDIRECT_URI)
+    print auth_req_url
+    raw_input('\nPress ENTER once you complete authorizing application ')
 
-#Obtain a request token from Pocket
-r = MakeRequest(REQ_TOK_METHOD_URL, BuildPayload(CONSUMER_KEY, REDIRECT_URI))
-REQUEST_TOKEN = RetInfUtil.GetRequestTokenFromResponse(r.text)
+    #Convert Request token to Access Token
+    payload = {'consumer_key': CONSUMER_KEY, 'code': REQUEST_TOKEN}
+    r = RetInfUtil.MakeRequest(ACCESS_TOK_REQ_URL, payload)
 
-#Generate Access Token Request URL. The URL directs users to a page where they can authenticate the app
-auth_req_url = RetInfUtil.BuildAccessTokReqURL(AUTHORIZE_REQ_URL, REQUEST_TOKEN, REDIRECT_URI)
-print auth_req_url
-raw_input("Authenticated? ")
+    ACCESS_INFO = RetInfUtil.ParseURLEncodedQuery(r.text)
+    ACCESS_INFO = {x: ACCESS_INFO[x][0] for x in ACCESS_INFO}
 
-#Convert Request token to Access Token
-payload = {'consumer_key': CONSUMER_KEY, 'code': REQUEST_TOKEN}
-r = MakeRequest(ACCESS_TOK_REQ_URL, payload)
-print r.text
+    if access_info_filename!='':
+        RetInfUtil.SaveAccessInfo(ACCESS_INFO, CONSUMER_KEY, access_info_filename)
+    RetInfUtil.SaveAccessInfo(ACCESS_INFO, CONSUMER_KEY)
+    return ACCESS_INFO
 
-#access_token=3c8b5ba3-58e8-fead-92d2-3427fb&username=humachine
-
+def main():
+    ACCESS_INFO = RetInfUtil.ReadAccessInfo()
+    print ACCESS_INFO
+    if ACCESS_INFO == '':
+        ACCESS_INFO = ObtainNewAccessToken()
--- RetrieveInfoUtil.py
+++ RetrieveInfoUtil.py
@@ -1,36 +1,55 @@
-from urlparse import urlparse, parse_qs
-def ReadConsumerKey(filename='pocket.apikeys'):
-    """Attempt to read consumer key from locally saved file.
+import requests
+import urlparse
+
+def MakeRequest(method_url, payload):
+    r = requests.post(method_url, data=payload)
+    return r
+
+def ReadAccessInfo(filename='pocket.apikeys'):
+    '''Attempt to read access info from locally saved file.
 
     Args:
         filename:   str, name of file containing consumer key
     Returns:
         consumer_key: str, consumer key of user
-    """
+    '''
+    print 'Trying to read file'
     try:
-        f = open("pocket.apikeys", "r")
-        consumer_key = f.read().strip()
+        f = open('pocket.apikeys', 'r')
+        cons_key = f.readline()
+        access_key = f.readline().strip()
+        username  = f.readline().strip()
+        if access_key == "" or username == "":
+            return ""
+        return {'access_key': access_key, 'username': username}
     except IOError:
-        consumer_key = "TESTKEY"
-    return consumer_key
+        return ""
 
-def GetRequestTokenFromResponse(response):
-    """Split response and obtain the request token.
+def ReadConsumerKey(filename='pocket.apikeys'):
+    '''Attempt to read consumer key from locally saved file.
 
     Args:
-        response: str, string containing HTTP response of request token request.
+        filename:   str, name of file containing consumer key
     Returns:
-        request_token: str, request token retrieved from response.
-    """
-    contents = response.split('=')
-    if len(contents) <= 1:
-        raise IOError
-    request_token = contents[1]
-    return request_token
+        consumer_key: str, consumer key of user
+    '''
+    try:
+        f = open('pocket.apikeys', 'r')
+        consumer_key = f.readline().strip()
+    except IOError:
+        consumer_key = 'TESTKEY'
+    return consumer_key
 
-def ParseURLEncodedToDict(url):
-    return parse_qs(urlparse(url).query)
+def ParseURLEncodedQuery(url_query):
+    return urlparse.parse_qs(url_query)
 
 def BuildAccessTokReqURL(url_no_params, request_token, redirect_uri):
     url_no_params = url_no_params.rstrip('?')
-    return url_no_params + "?" + "request_token=" + request_token + "&" + "redirect_uri=" + redirect_uri
+    return url_no_params + '?' + 'request_token=' + request_token + '&' + 'redirect_uri=' + redirect_uri
+
+def SaveAccessInfo(access_info, consumer_key, filename='pocket.apikeys'):
+    f = open(filename, 'w')
+    f.write(consumer_key + '\n')
+    f.write(access_info['access_token'] + '\n')
+    f.write(access_info['username'] + '\n')
+    f.close()
